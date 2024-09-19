d68a9a9dff5930ba630289f2856f962f5ba91026
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2024-09-19 23:49:01 +0330

    index page get active

--- kashangol/urls.py
+++ kashangol/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-from debug_toolbar import urls as debug_toolbar_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , include('website.urls')),
 ]
 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
