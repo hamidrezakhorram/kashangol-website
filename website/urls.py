38167baf774d9fd35933de5f37bc3cd60383f421
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2024-09-20 00:24:08 +0330

    header fixing
    the link of home, about, contact, shop , cart , terms of usage , questions got fixed

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,13 @@ app_name = 'website'
 
 urlpatterns = [
     path('', index_views , name ='index'),
+    path('shop' ,shop_views , name ='shop'),
+    path('articles' ,articles_views , name ='articles'),
+    path('about' , about_views , name ='about'),
+    path('contact' ,contact_views , name ='contact'),
+    path('cart' ,cart_views , name ='cart'),
+    path('questions' ,questions_views , name ='questions'),
+    path('terms' ,terms_views , name ='terms'),
+
     
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index_views(request):
     return render(request, 'index.html')
+
+
+def shop_views(request):
+    return render(request, 'shop/shop.html')
+
+def articles_views(request):
+    return render(request, 'articles/article.html')
+
+def about_views(request):
+    return render(request, 'about.html')
+
+def contact_views(request):
+    return render(request, 'contact.html')
+
+def cart_views(request):
+    return render(request, 'cart.html')
+def questions_views(request):
+    return render(request, 'F&Q.html')
+def terms_views(request):
+    return render(request, 'termusage.html')
+
