94c17a4820d8a03586ae05ed66f3a5c80c5f08f4
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2024-09-20 14:22:10 +0330

    create shop database

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from shop.models import Category, Tags, Product
 
+
+
+class ProducAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = 'empty'
+    list_display = ('name' ,'pub_status','published_date','created_date' , 'price' , 'discount')
+    list_filter = ('created_date', 'category' , 'tags' )
+    search_fields = ['name','content']
 # Register your models here.
+
+admin.site.register(Product , ProducAdmin)
+admin.site.register(Category)
+admin.site.register(Tags)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name =models.CharField(max_length = 255)
+
+    def __str__(self) -> str:
+        return self.name
+class Tags(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    brand_name = models.CharField(max_length=255 , null=True)
+    price = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+    content = models.TextField()
+    pub_status = models.BooleanField(default=True)
+    store_status = models.BooleanField(default=True)
+
+    published_date = models.DateField(null=True)
+    created_date = models.DateField(auto_now_add=True)
+    update_date= models.DateField(auto_now=True)
+    image = models.ImageField(upload_to= 'shop/' , default='shop/default.jpg')
+    category =models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tags)
+    
+
+
+    def __str__(self) -> str:
+        return self.name
+    
+
