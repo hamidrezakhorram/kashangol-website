71eb4943842f7ca7d3a487ce5d16a42de1c2aaad
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2024-09-21 23:51:00 +0330

    shop paginator created

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render , get_object_or_404
 from shop.models import Product ,Category
+from django.core.paginator import Paginator , PageNotAnInteger , EmptyPage
 
 def shop_views(request):
     products = Product.objects.filter( pub_status = True)
     category= Category.objects.all()
     for product in products:
        product.price = product.price - product.discount
-        
+    products=Paginator(products, 9) 
+    try:
+        page_number = request.GET.get("page")
+        products = products.page(page_number)
+    except PageNotAnInteger:
+        products = products.page(1)
+    except EmptyPage :
+        products = products.page(1)    
     context ={'products': products , 'category': category}
     return render(request, 'shop/shop.html' , context)
 
