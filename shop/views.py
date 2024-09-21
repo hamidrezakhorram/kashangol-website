c870876944a0e00b507ad801d40f5db97e74d1de
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2024-09-21 15:06:42 +0330

    changed main color from gr to red

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render , get_object_or_404
-from shop.models import Product
+from shop.models import Product ,Category
 
 def shop_views(request):
     products = Product.objects.filter( pub_status = True)
-   
+    category= Category.objects.all()
     for product in products:
        product.price = product.price - product.discount
         
-    context ={'products': products}
+    context ={'products': products , 'category': category}
     return render(request, 'shop/shop.html' , context)
 
 
