2bcba800ee16fa728d1ec134d77ea1ec340a8c3c
hamidkhorram <hamid.khorram.2020@gmail.com>
2024-10-07 20:05:48 +0330

    article index page fixed

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def articles_views(request):
-    return render(request, 'articles/article.html')
+    posts = Post.objects.filter(pub_status=True).order_by('published_date')
+    context = {'posts': posts}
+
+    return render(request, 'articles/article.html' , context)
 
 
 
