79883f36f84fad7ce67389996fcaabd751a7d37b
hamidkhorram <hamid.khorram.2020@gmail.com>
2024-10-10 00:37:00 +0330

    newset articles added to home page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render , HttpResponseRedirect
 from shop.models import Category , Product , Cartitem
+from articles.models import Post
+
 from website.forms import Newsletterform
 
 
@@ -10,11 +12,13 @@ def index_views(request):
     products = Product.objects.all()
     products_by_category = {category: Product.objects.filter(category__in=[category]) for category in categories}
     most_sell = Product.objects.filter(pub_status= True).exclude(sell_count = 0).order_by('-sell_count')[:6]  
+    posts = Post.objects.filter(pub_status= True).order_by('-created_date')[:4]
 
     context = {'categories': categories ,
                 'products': products ,
                 'products_by_category': products_by_category,
                 'most_sell' : most_sell,
+                'posts' : posts
 
 
              }
